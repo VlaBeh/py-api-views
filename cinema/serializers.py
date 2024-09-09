4e69d7701bbe0b0a2b104d994764f9ef526d64dd
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-09-09 14:21:23 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -11,8 +11,10 @@ class ActorSerializer(serializers.Serializer):
         return Actor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.first_name = validated_data.get("first_name",
+                                                 instance.first_name)
+        instance.last_name = validated_data.get("last_name",
+                                                instance.last_name)
         instance.save()
         return instance
 
@@ -40,9 +42,11 @@ class CinemaHallSerializer(serializers.Serializer):
         return CinemaHall.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
+        instance.name = validated_data.get("name",
+                                           instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
+        instance.seats_in_row = validated_data.get("seats_in_row",
+                                                   instance.seats_in_row)
         instance.save()
         return instance
 
@@ -51,8 +55,10 @@ class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
-    actors = serializers.PrimaryKeyRelatedField(queryset=Actor.objects.all(), many=True)
-    genres = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(), many=True)
+    genres = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(), many=True)
     duration = serializers.IntegerField()
 
     def create(self, validated_data):
--- cinema/views.py
+++ cinema/views.py
@@ -13,6 +13,7 @@ class GenreList(APIView):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = GenreSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
